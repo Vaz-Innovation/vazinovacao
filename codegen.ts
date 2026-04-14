b4a76e359349d3e77015daa6e96da44985e322e5
Manuel Antunes <manuelnascimento5589@gmail.com>
2026-04-14 01:18:35 -0300

    fix: use schema.graphql file to generate codegen schema

--- codegen.ts
+++ codegen.ts
@@ -1,19 +1,15 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: [
-    process.env.WORDPRESS_API_URL ||
-      process.env.NEXT_PUBLIC_WORDPRESS_API_URL ||
-      './src/graphql/schema.graphql',
-  ],
+  schema: ["./schema.graphql"],
   ignoreNoDocuments: true,
-  documents: ['src/**/*.{ts,tsx}'],
+  documents: ["src/**/*.{ts,tsx}"],
   generates: {
-    './src/graphql/__gen__/': {
-      preset: 'client',
+    "./src/graphql/__gen__/": {
+      preset: "client",
       plugins: [],
       config: {
-        documentMode: 'string',
+        documentMode: "string",
       },
     },
   },
